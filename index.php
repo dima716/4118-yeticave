e6067d17c81ed66f29a8ff9ab070c455438901eb
dima716 <dima716@yandex.ru>
2018-02-08 11:52:29 +0300

    change for to foreach

--- index.php
+++ index.php
@@ -119,25 +119,25 @@ $ads = [
       <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-      <?php for ($i = 0; $i < sizeof($ads); $i++) : ?>
+      <?php foreach ($ads as $ad) : ?>
         <li class="lots__item lot">
           <div class="lot__image">
-            <img src="<?= $ads[$i]['url'] ?>" width="350" height="260" alt="<?= $ads[$i]['name'] ?>">
+            <img src="<?= $ad['url'] ?>" width="350" height="260" alt="<?= $ad['name'] ?>">
           </div>
           <div class="lot__info">
-            <span class="lot__category"><?= $ads[$i]['category'] ?></span>
-            <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $ads[$i]['name'] ?></a></h3>
+            <span class="lot__category"><?= $ad['category'] ?></span>
+            <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $ad['name'] ?></a></h3>
             <div class="lot__state">
               <div class="lot__rate">
                 <span class="lot__amount">Стартовая цена</span>
-                <span class="lot__cost"><?= $ads[$i]['price'] ?><b class="rub">р</b></span>
+                <span class="lot__cost"><?= $ad['price'] ?><b class="rub">р</b></span>
               </div>
               <div class="lot__timer timer">
               </div>
             </div>
           </div>
         </li>
-      <?php endfor; ?>
+      <?php endforeach; ?>
     </ul>
   </section>
 </main>
@@ -145,11 +145,11 @@ $ads = [
 <footer class="main-footer">
   <nav class="nav">
     <ul class="nav__list container">
-      <?php for ($i = 0; $i < sizeof($categories); $i++) : ?>
+      <?php foreach ($categories as $category) : ?>
         <li class="nav__item">
-          <a href="all-lots.html"><?= $categories[$i] ?></a>
+          <a href="all-lots.html"><?= $category ?></a>
         </li>
-      <?php endfor; ?>
+      <?php endforeach; ?>
     </ul>
   </nav>
   <div class="main-footer__bottom container">
