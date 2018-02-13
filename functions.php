868b0544f0eedd861b3eb821a5ea44c02f2d8fa2
dima716 <dima716@yandex.ru>
2018-02-13 04:51:10 +0300

    add comments to functions

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * Format a number with grouped thousands and add currency sign
+ * @param $price
+ * @return string
+ */
 function format_price($price)
 {
   $price = number_format(ceil($price), 0, '.', ' ');
   return $price . ' ' . '₽';
 }
 
+/**
+ * Get content of template
+ * @param $template
+ * @param $vars
+ * @return string
+ */
 function include_template($template, $vars)
 {
   if (!file_exists($template)) {
