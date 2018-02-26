baadae539ec839599b027fc38903978e2e04e7c6
dima716 <dima716@yandex.ru>
2018-02-26 14:33:29 +0300

    show user's viewed lots on separate page

---
+++ history.php
@@ -0,0 +1,31 @@
+<?php
+require_once "data.php";
+require_once "functions.php";
+
+if (isset($_COOKIE["viewed_lots"])) {
+  $viewed_lots_indexes = json_decode($_COOKIE["viewed_lots"]);
+  $viewed_lots = [];
+
+  foreach ($viewed_lots_indexes as $viewed_lot_index) {
+    if (isset($ads[$viewed_lot_index])) {
+      // add index property for every viewed lot to form correct link to the lot
+      $ads[$viewed_lot_index]["index"] = $viewed_lot_index;
+      $viewed_lots[] = $ads[$viewed_lot_index];
+    }
+  }
+
+  $page_content = include_template("templates/history.php", ["viewed_lots" => $viewed_lots]);
+
+  $layout_content = include_template("templates/layout.php", [
+    "is_auth" => $is_auth,
+    "user_name" => $user_name,
+    "user_avatar" => $user_avatar,
+    "page_title" => "Главная",
+    "page_content" => $page_content,
+    "categories" => $categories
+  ]);
+
+  print($layout_content);
+} else {
+  print("You haven't viewed any lot yet.");
+}
---
+++ templates/history.php
@@ -0,0 +1,35 @@
+<section class="lots">
+  <h2>История просмотров</h2>
+  <ul class="lots__list">
+    <?php foreach ($viewed_lots as $viewed_lot) : ?>
+      <li class="lots__item lot">
+        <div class="lot__image">
+          <img src="<?= $viewed_lot["url"] ?>" width="350" height="260"
+            alt="<?= htmlspecialchars($viewed_lot["name"]) ?>">
+        </div>
+        <div class="lot__info">
+          <span class="lot__category"><?= $viewed_lot["category"] ?></span>
+          <h3 class="lot__title"><a class="text-link"
+              href="lot.php?id=<?= $viewed_lot["index"] ?>"><?= htmlspecialchars($viewed_lot["name"]) ?></a></h3>
+          <div class="lot__state">
+            <div class="lot__rate">
+              <span class="lot__amount">Стартовая цена</span>
+              <span class="lot__cost"><?= format_price($viewed_lot["rate"]) ?></span>
+            </div>
+            <div class="lot__timer timer">
+              <?= count_time_until_midnight() ?>
+            </div>
+          </div>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</section>
+<ul class="pagination-list">
+  <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
+  <li class="pagination-item pagination-item-active"><a>1</a></li>
+  <li class="pagination-item"><a href="#">2</a></li>
+  <li class="pagination-item"><a href="#">3</a></li>
+  <li class="pagination-item"><a href="#">4</a></li>
+  <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+</ul>
